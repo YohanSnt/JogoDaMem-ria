3404db5814d52a3c73512f06508030c212d93457
Yohan Snt <yohan.s@aluno.ifsp.edu.br>
2024-06-26 10:22:51 -0300

    Jogo da memória

---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿namespace JogoDaMemória
+{
+    internal class Player
+    {
+
+        //Atributos
+        private string _name;
+        private int _score;
+
+        //Métodos
+        public Player(string name)
+        {
+            Name = name;
+            _score = 0;
+        }
+
+        //Propriedade
+        public string Name
+        {
+            //receber o valor atibuído à propriedade(value).
+            set
+            {
+                if (value.Length >= 3)//tamanho da atribuição, quantidade de caracteres para que não fique vazio.
+                    _name = value;
+
+            }
+
+            //retorna o valor atribuído anteriormente caso seja "chamado".
+            get { return _name; }
+        }
+        public int Score
+        {
+            set
+            {
+                if (value > 0)
+                    _score = value;
+            }
+
+            get
+            {
+                return _score;
+            }
+        }
+        public override string ToString()
+        {
+            return "Nome: " + Name +
+                "\nPontução: " + Score;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,40 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void PrintMatrix(int[,] matrix)
+        {
+            Console.Write("   ");
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write(" {0}  ", j + 1);
+            }
+            Console.WriteLine("\n  -----------------");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("{0} |", i + 1);
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(" {0} |", matrix[i, j]);
+                }
+                Console.WriteLine("\n  -----------------");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
         {
             int[,] jogo = new int[4, 4];
             int[,] tela = new int[4, 4];
+            
+            Console.WriteLine("Nome do Player 1: ");
+            string nomeP1 = Console.ReadLine();
+
+            Console.WriteLine("Nome do Player 2: ");
+            string nomeP2 = Console.ReadLine();
+
+            Player p1 = new Player(nomeP1);
+            Player p2 = new Player(nomeP2);
+
 
             //Para criar números aleatórios
             Random gerador = new Random();
@@ -26,44 +56,36 @@
                 }
             }
 
-            int acertos = 0; 
+            //Sorteio jogador que começa
+            int jogador = gerador.Next(1,3);
+
+           
+
+            int acertos = 0;
             do
             {
-                for (int i = 0; i < 4; i++)
-                {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        Console.Write("{0}  |", tela[i, j]);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
+                Program.PrintMatrix(jogo);
+                Program.PrintMatrix(tela);
+                
 
                 //Pedir as posições do primeiro número
-                do {
-                    Console.WriteLine("Escolha uma linha para jogar [1, 4]: ");
-                    int lin1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Escolha uma linha para jogar [1, 4]: ");
+                int lin1 = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Escolha uma coluna para jogar [1, 4]: ");
-                    int col1 = int.Parse(Console.ReadLine());
-                } while (lin1 > 1 || lin2<4 && col1>1 || col2<4);
-                
-                lin1 = gerador.Next(0, 4);
-                col1 = gerador.Next(0, 4);
+                Console.WriteLine("Escolha uma coluna para jogar [1, 4]: ");
+                int col1 = int.Parse(Console.ReadLine());
+
+                lin1--;
+                col1--;
 
                 tela[lin1, col1] = jogo[lin1, col1];
 
-                for (int i = 0; i < 4; i++)
-                {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        Console.Write("{0} |", tela[i, j]);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
+                 Program.PrintMatrix(tela);
+
+
 
                 //Pedir as posições do segundo número
+                Program.PrintMatrix(jogo);
                 Console.WriteLine("Escolha uma linha para jogar [1, 4]: ");
                 int lin2 = int.Parse(Console.ReadLine());
 
@@ -75,15 +97,9 @@
 
                 tela[lin2, col2] = jogo[lin2, col2];
 
-                for (int i = 0; i < 4; i++)
-                {
-                    for (int j = 0; j < 4; j++)
-                    {
-                        Console.Write("{0} ", tela[i, j]);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
+                Program.PrintMatrix(tela);
+
+
 
                 //Em caso de acerto, a matriz tela permanece como está.
                 //Em caso de erro, precisamos voltar as posições para zero.
@@ -94,10 +110,17 @@
                 }
                 else
                 {
+                    if (jogador == 1)
+                        p1.Score += 1;
+                    else 
+                        p2.Score += 1;
                     acertos++;
                 }
 
             } while (acertos < 8);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine();
+            Console.WriteLine(p2.ToString());
         }
     }
 }
